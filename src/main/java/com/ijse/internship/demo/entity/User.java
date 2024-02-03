3b9f2158b2cf858edfdb71fd55b25de58ef5e1de
Chanuka <chanuka.weerakkody123@gmail.com>
2024-02-03 09:35:56 +0530

    create user interface

---
+++ src/main/java/com/ijse/internship/demo/entity/User.java
@@ -0,0 +1,43 @@
+package com.ijse.internship.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long guideId;
+    private String id;
+    private String display_name;
+    private String email;
+    private String given_name;
+    private String middle_name;
+    private String name;
+    private String family_name;
+    private String nickname;
+    private String phoneNumber;
+    private String comment;
+    private int directory_id;
+    private int picture_id;
+    private String avatar_url;
+    private Object metaData;
+    @ElementCollection
+    private List<String> tags;
+
+    private String presence;
+    private String created_at;
+    private String modified_at;
+    private boolean is_supported;
+    private boolean is_trashed;
+}
