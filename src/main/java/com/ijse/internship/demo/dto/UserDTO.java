800b0284bb2f47f5fb6323e08b716a64c4c5ff65
Chanuka <chanuka.weerakkody123@gmail.com>
2024-02-03 09:37:22 +0530

    create user dto

---
+++ src/main/java/com/ijse/internship/demo/dto/UserDTO.java
@@ -0,0 +1,37 @@
+package com.ijse.internship.demo.dto;
+
+import jakarta.persistence.ElementCollection;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class UserDTO {
+    private Long guideId;
+    private String id;
+    private String display_name;
+    private String email;
+    private String given_name;
+    private String middle_name;
+    private String name;
+    private String family_name;
+    private String nickname;
+    private String phoneNumber;
+    private String comment;
+    private int directory_id;
+    private int picture_id;
+    private String avatar_url;
+    private Object metaData;
+    private List<String> tags;
+    private String presence;
+    private String created_at;
+    private String modified_at;
+    private boolean is_supported;
+    private boolean is_trashed;
+}
